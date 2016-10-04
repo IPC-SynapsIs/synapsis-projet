05805f08f3de245ec725cfb188bea47a874b1ab5
Blitzfis <mehdi.alaoui@capgemini.com>
2016-10-05 00:20:53 +0200

    test

--- src/main/java/org/ipc/synapsis/projet/ProjetServerClass.java
+++ src/main/java/org/ipc/synapsis/projet/ProjetServerClass.java
@@ -2,10 +2,12 @@ package org.ipc.synapsis.projet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
 @EnableJpaRepositories(basePackages = {"org.ipc.synapsis.projet.repository"})
 public class ProjetServerClass {
 
