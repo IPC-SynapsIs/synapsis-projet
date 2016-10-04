b515b8b6075354255f80d873105984c120323577
Blitzfis <mehdi.alaoui@capgemini.com>
2016-10-05 01:10:12 +0200

    test  @Configuration @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})

--- src/main/java/org/ipc/synapsis/projet/ProjetServerClass.java
+++ src/main/java/org/ipc/synapsis/projet/ProjetServerClass.java
@@ -1,11 +1,15 @@
 package org.ipc.synapsis.projet;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @EnableJpaRepositories(basePackages = {"org.ipc.synapsis.projet.repository"})
 public class ProjetServerClass {
 
